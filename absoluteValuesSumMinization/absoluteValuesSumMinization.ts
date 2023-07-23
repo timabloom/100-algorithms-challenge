c54e2a5e45bc35f91221d63add9237d1be73be3f
timabloom <timabloom@proton.me>
2023-07-23 21:49:37 +0200

    solved challenge 1

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,16 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+  const isEven = a.length % 2 === 0;
+  if (isEven) {
+    const medianLeft = a[a.length / 2 - 1];
+    return medianLeft;
+  } else {
+    const index = Math.floor(a.length / 2);
+    const median = a[index];
+    return median;
+  }
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
