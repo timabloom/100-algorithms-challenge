a3b39949632ee94fe0862ad70eece2e0a7aae657
timabloom <timabloom@proton.me>
2023-07-26 22:19:42 +0200

    solved challenge 2

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,16 @@
 export function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
-
+  let sum = 0;
+  param1.forEach((num) => {
+    sum += num;
+  });
+  return sum;
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
