b2f6e1a9c730a09002364ec4c6ab55be8a9b5a33
timabloom <timabloom@proton.me>
2023-07-27 17:26:19 +0200

    solved challenge 3

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,20 @@
 export function addBorder(picture: string[]): string[] {
+  const borderLength = picture[0].length + 2;
+  let border = "";
 
+  for (let i = 0; i < borderLength; i++) {
+    border = border.concat("*");
+  }
+
+  let pictureBorder: string[] = [];
+
+  picture.forEach((data) => {
+    pictureBorder.push(`*${data}*`);
+  });
+  pictureBorder.unshift(border);
+  pictureBorder.push(border);
+
+  return pictureBorder;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
