65d03739c0127926e257f904b70bdef49f987508
timabloom <timabloom@proton.me>
2023-07-27 19:02:28 +0200

    solved challenge 5

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,14 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  const productsArray: number[] = [];
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    productsArray.push(inputArray[i] * inputArray[i + 1]);
+  }
+  productsArray.sort();
+
+  const largestProduct = productsArray[productsArray.length - 1];
+
+  return largestProduct;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
