8d2de7a3f5b07d5635295575a6602b224546b2e7
timabloom <timabloom@proton.me>
2023-07-27 18:00:02 +0200

    solved challenge 4

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,13 @@
 export function addTwoDigits(n: any): number {
+  const digitsArray: string[] = n.toString().split("");
 
+  let sum = 0;
+
+  digitsArray.forEach((digit) => {
+    sum += parseInt(digit);
+  });
+
+  return sum;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
