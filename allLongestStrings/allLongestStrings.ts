0046f1d83c2f1c0206ddba3253489be57e80f282
timabloom <timabloom@proton.me>
2023-08-17 20:22:52 +0200

    solved challenge 6

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,16 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestString = 0;
+  inputArray.forEach((letters) => {
+    if (letters.length > longestString) {
+      longestString = letters.length;
+    }
+  });
 
+  const onlyLongestStrings = inputArray.filter(
+    (letters) => letters.length === longestString
+  );
+
+  return onlyLongestStrings;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
