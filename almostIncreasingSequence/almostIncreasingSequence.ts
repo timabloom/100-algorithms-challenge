48f9eca786265ff5497aa99b769e0625fec517da
timabloom <timabloom@proton.me>
2023-11-17 16:11:40 +0100

    solved challenge 7

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,20 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+  let removalCount = 0;
 
+  for (let i = 0; i < sequence.length - 1; i++) {
+    if (sequence[i] >= sequence[i + 1]) {
+      removalCount++;
+      if (removalCount > 1) {
+        return false;
+      }
+      if (sequence[i + 1] === sequence[i - 1]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
