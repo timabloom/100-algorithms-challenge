854b629d0ff5e091116592a47aa57f3f6d3198aa
timabloom <timabloom@proton.me>
2023-11-17 21:03:11 +0100

    solved challenge 7 fix

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -20,19 +20,3 @@ export function almostIncreasingSequence(sequence: number[]): boolean {
 
 console.log(almostIncreasingSequence([1, 3, 2, 1]));
 console.log(almostIncreasingSequence([1, 3, 2]));
-console.log(almostIncreasingSequence([1, 3, 1, 4]));
-console.log(almostIncreasingSequence([1, 3, 1, 2]));
-
-console.log(almostIncreasingSequence([1, 1, 2, 3])); // Should return true
-console.log(almostIncreasingSequence([4, 3, 2, 1])); // Should return false
-console.log(almostIncreasingSequence([1, 2, 3, 10000])); // Should return true
-console.log(almostIncreasingSequence([10000, 1, 2, 3])); // Should return true
-console.log(almostIncreasingSequence([1, 2, 2, 3, 4])); // Should return true
-
-console.log(almostIncreasingSequence([1, 2, 5, 3, 4, 6])); // Should return true
-
-console.log(almostIncreasingSequence([3, 1, 2, 4])); // Should return true
-console.log(almostIncreasingSequence([1, 2, 3, 0])); // Should return true
-console.log(almostIncreasingSequence([1, 3, 2, 1, 0])); // Should return false
-console.log(almostIncreasingSequence([1])); // Should return true
-console.log(almostIncreasingSequence([2, 1])); // Should return true
