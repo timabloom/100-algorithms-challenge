77f4113861fe75bbe2f6ae9c7c23456005067c74
timabloom <timabloom@proton.me>
2023-11-17 21:01:57 +0100

    solved challenge fix

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -7,14 +7,32 @@ export function almostIncreasingSequence(sequence: number[]): boolean {
       if (removalCount > 1) {
         return false;
       }
-      if (sequence[i + 1] === sequence[i - 1]) {
+      if (
+        sequence[i + 1] === sequence[i - 1] &&
+        sequence[i] >= sequence[i + 2]
+      ) {
         return false;
       }
     }
   }
-
   return true;
 }
 
 console.log(almostIncreasingSequence([1, 3, 2, 1]));
 console.log(almostIncreasingSequence([1, 3, 2]));
+console.log(almostIncreasingSequence([1, 3, 1, 4]));
+console.log(almostIncreasingSequence([1, 3, 1, 2]));
+
+console.log(almostIncreasingSequence([1, 1, 2, 3])); // Should return true
+console.log(almostIncreasingSequence([4, 3, 2, 1])); // Should return false
+console.log(almostIncreasingSequence([1, 2, 3, 10000])); // Should return true
+console.log(almostIncreasingSequence([10000, 1, 2, 3])); // Should return true
+console.log(almostIncreasingSequence([1, 2, 2, 3, 4])); // Should return true
+
+console.log(almostIncreasingSequence([1, 2, 5, 3, 4, 6])); // Should return true
+
+console.log(almostIncreasingSequence([3, 1, 2, 4])); // Should return true
+console.log(almostIncreasingSequence([1, 2, 3, 0])); // Should return true
+console.log(almostIncreasingSequence([1, 3, 2, 1, 0])); // Should return false
+console.log(almostIncreasingSequence([1])); // Should return true
+console.log(almostIncreasingSequence([2, 1])); // Should return true
