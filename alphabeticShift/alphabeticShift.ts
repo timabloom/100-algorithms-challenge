178e3abcf84d20c9b43603e45868c2ecaa37defd
timabloom <timabloom@proton.me>
2023-11-20 11:42:21 +0100

    solved challenge 8

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,16 @@
 export function alphabeticShift(inputString: string): string {
+  let newString = "";
 
+  for (let i = 0; i < inputString.length; i++) {
+    const UTFCode = inputString.charCodeAt(i);
+    if (UTFCode === 122) {
+      newString = newString + "a";
+    } else {
+      const newChar = String.fromCharCode(UTFCode + 1);
+      newString = newString + newChar;
+    }
+  }
+  return newString;
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));
