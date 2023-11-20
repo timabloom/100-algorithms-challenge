94234e2e7bf9466660264e83941da93aa46e1c7b
timabloom <timabloom@proton.me>
2023-11-20 14:34:56 +0100

    solved challenge 12

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,28 @@
 export function areSimilar(a: number[], b: number[]): boolean {
-
+  const sumOfA = a.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const sumOfB = b.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  if (sumOfA !== sumOfB) {
+    return false;
+  }
+  
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      count = count + 1;
+      if (count > 2) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
