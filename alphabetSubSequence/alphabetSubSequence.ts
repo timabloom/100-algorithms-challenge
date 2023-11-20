67cbb99c641acfb9073a5f48ecbbe2f2d8812028
timabloom <timabloom@proton.me>
2023-11-20 12:01:56 +0100

    solved challenge 9

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,19 @@
 export function alphabetSubsequence(s: string): boolean {
+  const inputLowerCase = s.toLowerCase();
+  let UTFCodeValue = inputLowerCase.charCodeAt(0);
 
+  for (let i = 1; i < inputLowerCase.length; i++) {
+    const UTFCodeNextValue = inputLowerCase.charCodeAt(i);
+    if (UTFCodeValue >= UTFCodeNextValue) {
+      return false;
+    }
+    UTFCodeValue = UTFCodeNextValue;
+  }
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
