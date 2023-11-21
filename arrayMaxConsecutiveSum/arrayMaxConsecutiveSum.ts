d1fdae0764eb4664d77b639309d8c30e48de66e1
timabloom <timabloom@proton.me>
2023-11-21 20:26:41 +0100

    solved challenge 15

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,19 @@
-export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+export function arrayMaxConsecutiveSum(
+  inputArray: number[],
+  k: number
+): number {
+  let maxSum = 0;
+  for (let i = 0; i < inputArray.length - k + 1; i++) {
+    const slicedArray = inputArray.slice(i, i + k);
+    const nextSum = slicedArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    if (maxSum < nextSum) {
+      maxSum = nextSum;
+    }
+  }
+  return maxSum;
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
