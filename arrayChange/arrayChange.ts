fa6c08a4dc43775edc366a6cea4afbcc5730ff5c
timabloom <timabloom@proton.me>
2023-11-21 18:16:50 +0100

    solved challenge 13

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,15 @@
 export function arrayChange(inputArray: number[]): number {
-
+  let count = 0;
+  let previousNumber = inputArray[0];
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= previousNumber) {
+      count += previousNumber - inputArray[i] + 1;
+      previousNumber = previousNumber + 1;
+    } else {
+      previousNumber = inputArray[i];
+    }
+  }
+  return count;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
