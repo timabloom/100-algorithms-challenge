fd122cc73510098f3c1a420342ee82f9b8dedae4
timabloom <timabloom@proton.me>
2023-11-22 16:12:41 +0100

    solved challenge 19

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,20 @@
 export function avoidObstacles(inputArray: number[]): number {
+  const maxValue = Math.max(...inputArray);
+  let jumpLength = 1;
+  let jumpSuccess = false;
 
+  while (!jumpSuccess) {
+    jumpSuccess = true;
+    for (let i = 0; i <= maxValue; i += jumpLength) {
+      if (inputArray.includes(i)) {
+        jumpLength++;
+        jumpSuccess = false;
+        break;
+      }
+    }
+  }
+
+  return jumpLength;
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
