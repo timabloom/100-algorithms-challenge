ec4f3afbebef87aa4d59c9d95ea579c0a14a9c08
timabloom <timabloom@proton.me>
2023-11-22 15:01:36 +0100

    solved challenge 18

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,14 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === elemToReplace) {
+      inputArray[i] = substitutionElem;
+    }
+  }
+  return inputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
