8d3741334f22f7fcdaf575d54d7d38c386ddcdfa
timabloom <timabloom@proton.me>
2023-11-22 14:23:56 +0100

    solved challenge 16

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,10 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+  let maxDiff = 0;
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    const currentDiff = Math.abs(inputArray[i] - inputArray[i + 1]);
+    maxDiff = Math.max(currentDiff, maxDiff);
+  }
+  return maxDiff;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
