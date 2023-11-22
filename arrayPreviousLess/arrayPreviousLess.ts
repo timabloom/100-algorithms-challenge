033a646eb8fd77c8a4203e59a21f98109c6e9dc5
timabloom <timabloom@proton.me>
2023-11-22 14:53:31 +0100

    solved challenge 17

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,16 @@
 export function arrayPreviousLess(items: number[]): number[] {
-
+  let newArray: number[] = [];
+  for (let i = 0; i < items.length; i++) {
+    for (let j = i; j >= 0; j--) {
+      if (items[i] > items[j]) {
+        newArray.push(items[j]);
+        break;
+      } else if (j === 0) {
+        newArray.push(-1);
+      }
+    }
+  }
+  return newArray;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
