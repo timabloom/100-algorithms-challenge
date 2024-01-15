b2829f56936b2c19673206a94abfade003f3d712
timabloom <timabloom@proton.me>
2024-01-15 13:33:57 +0100

    solved challenge 27

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,6 +1,8 @@
 export function chessBoardCellColor(cell1: string, cell2: string): boolean {
-
+  const cell1Sum = cell1.toLowerCase().charCodeAt(0) + parseInt(cell1[1]);
+  const cell2Sum = cell2.toLowerCase().charCodeAt(0) + parseInt(cell2[1]);
+  return cell1Sum % 2 === cell2Sum % 2;
 }
 
-// console.log(chessBoardCellColor('A1', 'C3'));
-// console.log(chessBoardCellColor('A1', 'H3'));
+console.log(chessBoardCellColor("A1", "C3"));
+console.log(chessBoardCellColor("A1", "H3"));
