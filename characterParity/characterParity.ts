a2a02702712cf5cbfe2508f90f1e037f68b97c5d
timabloom <timabloom@proton.me>
2024-01-15 13:03:50 +0100

    solved challenge 25

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,13 @@
 export function characterParity(symbol: string): string {
-
+  const digit = parseInt(symbol);
+  const digitNaN = isNaN(digit);
+  if (digitNaN) {
+    return "not a digit";
+  } else if (Number.isInteger(digit / 2)) {
+    return "even";
+  } else return "odd";
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity("5"));
+console.log(characterParity("8"));
+console.log(characterParity("q"));
