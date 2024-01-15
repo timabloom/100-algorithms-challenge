3eb9fa165b78b0ac0ac1a03c7b3c0dce51c73fa5
timabloom <timabloom@proton.me>
2024-01-15 12:37:23 +0100

    solved challenge 23

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,12 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  const lowerCase = inputString.toLowerCase();
+  const lowerCaseReversed = lowerCase.split("").reverse().join("");
+  if (lowerCase === lowerCaseReversed) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
