ca725c7359bb21080f21d519969b15e46180873c
timabloom <timabloom@proton.me>
2024-01-15 12:52:18 +0100

    solved challenge 24

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,6 @@
 export function centuryFromYear(year: number): number {
-
+  return Math.ceil(year / 100);
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
