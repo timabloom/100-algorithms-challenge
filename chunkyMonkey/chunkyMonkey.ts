734349c76612794bb3bce0e3fe26453c415925e9
timabloom <timabloom@proton.me>
2024-01-15 14:11:17 +0100

    solved challenge 28

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,12 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
+  let newArray: any[][] = [];
 
+  for (let i = 0; i < arr.length; i = i + size) {
+    newArray.push(arr.slice(i, i + size));
+  }
+
+  return newArray;
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
