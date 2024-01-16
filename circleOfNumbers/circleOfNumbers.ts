9404afc72ef58c8d2e70cd2f15e96145f10987e4
timabloom <timabloom@proton.me>
2024-01-16 14:49:27 +0100

    solved challenge 29

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,14 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
+  let circleOfNumbers: number[] = [];
+  for (let i = 0; i < n; ++i) {
+    circleOfNumbers.push(i);
+  }
 
+  if (n / 2 > firstNumber) {
+    return circleOfNumbers[firstNumber + n / 2]
+  } else {
+    return circleOfNumbers[firstNumber - n / 2]
+  }
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 9));
