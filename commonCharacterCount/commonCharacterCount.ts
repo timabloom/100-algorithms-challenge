5ed3cc8d029191b36e9d7c27397d7893658fcae4
timabloom <timabloom@proton.me>
2024-01-16 15:31:17 +0100

    solved challenge 30

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,17 @@
 export function commonCharacterCount(s1: string, s2: string): number {
+  const array1 = s1.split("");
+  const array2 = s2.split("");
 
+  let count = 0;
+
+  for (let i = 0; i < s1.length; i++) {
+    const index = array2.findIndex((element) => element === array1[i]);
+    if (index > -1) {
+      array2.splice(index, 1);
+      count++;
+    }
+  }
+  return count;
 }
 
-// console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount("aabcc", "adcaa"));
