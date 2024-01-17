ea862ef40acf6f5bd1df424b907ef1981f8bf73f
timabloom <timabloom@proton.me>
2024-01-17 17:51:38 +0100

    solved challenge 33

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,5 +1,19 @@
 export function composeRanges(nums: number[]): string[] {
-
+  let composeRanges: string[] = [];
+  let range = "";
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.length - 1 === i) {
+      range += `${nums[i]}`;
+      composeRanges.push(range);
+    } else if (range === "") {
+      range += `${nums[i]}`;
+    } else if (nums[i] + 1 !== nums[i + 1]) {
+      range += `->${nums[i]}`;
+      composeRanges.push(range);
+      range = "";
+    }
+  }
+  return composeRanges;
 }
 
-// console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
