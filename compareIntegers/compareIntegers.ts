c92c87abb20fbec001a588d6998db5d8a215a78d
timabloom <timabloom@proton.me>
2024-01-17 16:58:23 +0100

    solved challenge 32

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,12 @@
 export function compareIntegers(a: string, b: string): string {
-
+  if (a > b) {
+    return "greater";
+  } else if (a < b) {
+    return "less";
+  }
+  return "equal";
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers("12", "13"));
+console.log(compareIntegers("875", "799"));
+console.log(compareIntegers("1000", "1000"));
