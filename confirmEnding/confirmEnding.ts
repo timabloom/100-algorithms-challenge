b75d6fe71a681769fa4911439f94c134c607c0f8
timabloom <timabloom@proton.me>
2024-01-17 18:18:20 +0100

    solved challenge 34

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,12 @@
 export function confirmEnding(str: string, target: string) {
-
+  for (let i = str.length; i > 0; i--) {
+    const endStr = str.substring(i - 1, str.length);
+    if (endStr === target) {
+      return true;
+    }
+  }
+  return false;
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
