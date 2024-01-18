1516d9b954ba22d68049c152c7329da07cfb1b2b
timabloom <timabloom@proton.me>
2024-01-18 16:02:34 +0100

    solved challenge 35

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,14 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
-
+  let indices = {};
+  for (let i = 0; i < nums.length; i++) {
+    indices[i] = nums[i];
+    for (let j = 0; j < i; j++) {
+      if (indices[j] === nums[i]) {
+        return Math.abs(j - i) === k;
+      }
+    }
+  }
 }
 
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
