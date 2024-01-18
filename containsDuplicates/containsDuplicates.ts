374bb4ef84be18e3a10429cf5f20fe3c21cfccb6
timabloom <timabloom@proton.me>
2024-01-18 16:20:25 +0100

    solved challenge 36

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,11 @@
 export function containsDuplicates(a: number[]): boolean {
-
+  for (let i = 0; i < a.length; i++) {
+    const value = a[i];
+    a.shift();
+    return a.includes(value) === true;
+  }
+  return false;
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));
