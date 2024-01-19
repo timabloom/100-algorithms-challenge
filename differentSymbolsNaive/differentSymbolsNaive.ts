bd2294702ba3d98ff8497ffe2aaab971db28c787
timabloom <timabloom@proton.me>
2024-01-19 16:33:52 +0100

    solved challenge 41

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,12 @@
 export function differentSymbolsNaive(s: string): number {
-
+  const allCharacters: string[] = s.split("");
+  const uniqueCharacters: string[] = [];
+  for (let i = 0; i < allCharacters.length; i++) {
+    if (!uniqueCharacters.includes(allCharacters[i])) {
+      uniqueCharacters.push(allCharacters[i]);
+    }
+  }
+  return uniqueCharacters.length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive("cabca"));
