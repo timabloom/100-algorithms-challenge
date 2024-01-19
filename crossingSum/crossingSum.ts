a98c9f7542e99dcee3e5d5bb1e52e837a732246f
timabloom <timabloom@proton.me>
2024-01-19 15:51:58 +0100

    solved challenge 39

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,22 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
-
+  const row = matrix[a];
+  const rowSkip = matrix[a][b];
+  let column: number[] = [];
+  for (let i = 0; i < matrix.length; i++) {
+    column.push(matrix[i][b]);
+  }
+  const allCells = row.concat(column);
+  return allCells.reduce((acc, curr) => acc + curr, 0) - rowSkip;
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(
+  crossingSum(
+    [
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3],
+    ],
+    1,
+    3
+  )
+);
