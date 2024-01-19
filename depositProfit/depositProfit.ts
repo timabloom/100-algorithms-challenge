709caad4f21e7024e9ebf956d13dbf709503e605
timabloom <timabloom@proton.me>
2024-01-19 16:04:56 +0100

    solved challenge 40

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,14 @@
-export function depositProfit(deposit: number, rate: number, threshold: number): number {
-
+export function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let year = 0;
+  while (deposit < threshold) {
+    deposit += deposit * (rate / 100);
+    year += 1;
+  }
+  return year;
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
