65c1f63459408bef6f229dba99f9dcaa9fba8dc9
timabloom <timabloom@proton.me>
2024-01-19 17:14:15 +0100

    solved challenge 42

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,18 @@
 export function digitDegree(n: number): number {
-
+  const stringDigits = n.toString().split("");
+  let digits = stringDigits.map((string) => parseInt(string));
+  let count = 0
+  while (digits.length > 1) {
+      const sum = digits
+        .reduce((acc, curr) => acc + curr, 0)
+        .toString()
+        .split("");
+      digits = sum.map((string) => parseInt(string));
+      count++
+  }
+  return count
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+console.log(digitDegree(5));
+console.log(digitDegree(100));
+console.log(digitDegree(91));
