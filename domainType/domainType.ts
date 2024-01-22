b68cca788c6f214a775de2b117fd64839a4b0e7b
timabloom <timabloom@proton.me>
2024-01-22 13:47:29 +0100

    solved challenge 43

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,22 @@
 export function domainType(domains: string[]): string[] {
-
+  const domainTypes = [
+    ["organization", "org"],
+    ["commercial", "com"],
+    ["network", "net"],
+    ["information", "info"],
+  ];
+  const chosenDomains: string[] = [];
+  for (let i = 0; i < domains.length; i++) {
+    const domainParts = domains[i].split(".");
+    for (let i = 0; i < domainTypes.length; i++) {
+      if (domainParts.includes(domainTypes[i][1])) {
+        chosenDomains.push(domainTypes[i][0]);
+      }
+    }
+  }
+  return chosenDomains;
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"])
+);
