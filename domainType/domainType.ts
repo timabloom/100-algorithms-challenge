b7249f46c1660773ad38a86eacb62d2aad94b290
timabloom <timabloom@proton.me>
2024-01-22 13:52:06 +0100

    solved challenge 43 fix

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -8,8 +8,9 @@ export function domainType(domains: string[]): string[] {
   const chosenDomains: string[] = [];
   for (let i = 0; i < domains.length; i++) {
     const domainParts = domains[i].split(".");
+    const lastDomain = domainParts[domainParts.length - 1];
     for (let i = 0; i < domainTypes.length; i++) {
-      if (domainParts.includes(domainTypes[i][1])) {
+      if (lastDomain.includes(domainTypes[i][1])) {
         chosenDomains.push(domainTypes[i][0]);
       }
     }
