3b145cd3d159dbdd63e625531c566ff557eb192c
timabloom <timabloom@proton.me>
2024-01-22 14:45:05 +0100

    solved challenge 48

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,21 @@
-export function extractMatrixColumn(matrix: number[][], column: number): number[] {
-
+export function extractMatrixColumn(
+  matrix: number[][],
+  column: number
+): number[] {
+  const elementNumbers: number[] = [];
+  for (let i = 0; i < matrix.length; i++) {
+    elementNumbers.push(matrix[i][column]);
+  }
+  return elementNumbers;
 }
 
-// console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(
+  extractMatrixColumn(
+    [
+      [1, 1, 1, 2],
+      [0, 5, 0, 4],
+      [2, 1, 3, 6],
+    ],
+    2
+  )
+);
