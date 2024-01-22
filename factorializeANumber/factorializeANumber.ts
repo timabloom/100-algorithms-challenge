4b00038e85f4fbf1c6cdf9aab814a6963aeff7a3
timabloom <timabloom@proton.me>
2024-01-22 14:57:56 +0100

    solved challenge 49

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,11 @@
 export function factorializeANumber(num: number): number {
-
+  let sum = 1;
+  if (num === 0) return 0;
+  for (let i = 1; i < num + 1; i++) {
+    sum *= i;
+  }
+  return sum;
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
