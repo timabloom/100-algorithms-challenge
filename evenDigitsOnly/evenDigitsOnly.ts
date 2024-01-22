87ff656a987794eedd585b036ad107f61b6ce084
timabloom <timabloom@proton.me>
2024-01-22 14:27:42 +0100

    solved challenge 46

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,7 @@
 export function evenDigitsOnly(n: number): boolean {
-
+  const arrayStringDigits = n.toString().split("");
+  return arrayStringDigits.every((digit) => parseInt(digit) % 2 === 0);
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));
