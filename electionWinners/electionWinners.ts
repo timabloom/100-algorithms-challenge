91ee2dca4ed05f72a0396f5c81a8008557d20ade
timabloom <timabloom@proton.me>
2024-01-22 14:20:15 +0100

    solved challenge 44

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,21 @@
 export function electionsWinners(votes: number[], k: number): number {
+  let mostVotes = 0;
+  votes.filter((votes) => {
+    if (votes > mostVotes) mostVotes += votes;
+  });
+  const mostVotesEqual = votes.filter((votes) => votes === mostVotes);
+  if (mostVotesEqual.length > 1) {
+    return 0;
+  }
+  const MaxVotesArray: number[] = [];
+  for (let i = 0; i < votes.length; i++) {
+    const maxVotes = (votes[i] += k);
+    if (maxVotes > mostVotes) {
+      MaxVotesArray.push(maxVotes);
+    }
+  }
 
+  return MaxVotesArray.length;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
