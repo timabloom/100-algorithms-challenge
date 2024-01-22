bc605c0f545ee9b4e3a95b40dc93b3f1f3d61121
timabloom <timabloom@proton.me>
2024-01-22 14:36:59 +0100

    solved challenge 47 fix

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,5 +1,5 @@
 export function extractEachKth(inputArray: number[], k: number): number[] {
-  return inputArray.filter((element) => element !== k);
+  return inputArray.filter((_element, index) => (index + 1) % k !== 0);
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
