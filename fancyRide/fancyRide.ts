235bbaf416d094bf588767d42edbaaa5aaf373c9
timabloom <timabloom@proton.me>
2024-02-03 15:26:44 +0100

    solved challenge 50

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,11 @@
 export function fancyRide(l: number, fares: number[]): string {
-
+  const ubers = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+  for (let i = fares.length - 1; i >= 0; i--) {
+    const cost = fares[i] * l;
+    if (cost <= 20) {
+      return ubers[i];
+    }
+  }
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
