3766285b2e38d3f695643e31ac484a1046f7819e
timabloom <timabloom@proton.me>
2024-02-05 19:17:27 +0100

    solved challenge 51

--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,5 +1,17 @@
-export function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
+export function fareEstimator(
+  ride_time: number,
+  ride_distance: number,
+  cost_per_minute: number[],
+  cost_per_mile: number[]
+): number[] {
+  const rideCosts: number[] = [];
 
+  for (let i = 0; i < cost_per_mile.length; i++) {
+    const cost =
+      cost_per_minute[i] * ride_time + cost_per_mile[i] * ride_distance;
+    rideCosts.push(parseFloat(cost.toFixed(1)));
+  }
+  return rideCosts;
 }
 
-// console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
+console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
