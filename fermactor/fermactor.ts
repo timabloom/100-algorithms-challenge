dcb1f1fbc9ab45f05ed03c3fd735aa7f5cb06061
timabloom <timabloom@proton.me>
2024-02-06 16:03:57 +0100

    solved challenge 52

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,16 @@
 export function fermactor(n: number): number[] {
-
+  const arrayCD: number[] = [];
+  let c = 0;
+  while (c !== n) {
+    for (let i = 0; i < n; i++) {
+      if (Math.pow(c, 2) - Math.pow(i, 2) === n) {
+        arrayCD.push(c, i);
+        return arrayCD;
+      }
+    }
+    c++;
+  }
+  return arrayCD;
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));
