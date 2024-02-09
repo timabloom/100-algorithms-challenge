7061c63ca2bb0da143748dc01d8ac329c323f6a5
timabloom <timabloom@proton.me>
2024-02-09 17:41:20 +0100

    solved challenge 53 fix

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,7 @@
 export function findEmailDomain(address: string): string {
-
+  const index = address.lastIndexOf("@");
+  return address.slice(index + 1);
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain("prettyandsimple@example.com"));
+console.log(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org'));
