71b892b752097ef2463ce13b92751aa1a1f27feb
timabloom <timabloom@proton.me>
2024-02-09 17:24:59 +0100

    solved challenge 53

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,6 +1,16 @@
 export function findClosestPair(numbers: number[], sum: number): number {
-
+  let pairIndiceDiff = -1;
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; ++j) {
+      if (numbers[i] + numbers[j] === sum && Math.abs(i - j) < pairIndiceDiff) {
+        pairIndiceDiff = Math.abs(i - j);
+      } else if (numbers[i] + numbers[j] === sum) {
+        pairIndiceDiff = Math.abs(i - j);
+      }
+    }
+  }
+  return pairIndiceDiff;
 }
 
-// console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
-// console.log(findClosestPair([2, 3, 7], 8));
+console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
+console.log(findClosestPair([2, 3, 7], 8));
