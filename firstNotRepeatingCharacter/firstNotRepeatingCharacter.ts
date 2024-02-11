b478fc62ea770932fc93f0e24ec5325334123925
timabloom <timabloom@proton.me>
2024-02-11 20:23:57 +0100

    solved challenge 56

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,23 @@
 export function firstNotRepeatingCharacter(s: string): string {
+  const charArray = s.split("");
+  const charObject = {};
+  let index = -1;
+  for (let i = 0; i < s.length; i++) {
+    const char = charArray[i];
+    if (!charObject.hasOwnProperty(charArray[i])) {
+      charObject[char] = 1;
+    } else {
+      charObject[char]++;
+    }
+    if (charObject[char] === 1 && index === -1) {
+      index = i;
+    } else if (charObject[char] > 1 && char === charArray[index]) {
+      index = -1;
+    }
+  }
 
+  return index === -1 ? "_" : charArray[index];
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter("abacabad"));
+console.log(firstNotRepeatingCharacter("abacabaabacaba"));
