0f458d2e3fda86e5f7081d6faf6e1a102ae5aecd
timabloom <timabloom@proton.me>
2024-02-18 22:44:54 +0100

    solved challenge 57

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,16 @@
 export function flattenArray(arr: any[]): any[] {
-
+  const flatArrayStart: any[] = arr.flat();
+  const flatArrayFinal: any[] = [];
+  for (let i = 0; i < flatArrayStart.length; i++) {
+    if (Array.isArray(flatArrayStart[i])) {
+      const flatValue = flatArrayStart[i].flat();
+      flatArrayFinal.push(flatValue[0]);
+    } else {
+      flatArrayFinal.push(flatArrayStart[i]);
+    }
+  }
+  return flatArrayFinal;
 }
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));
