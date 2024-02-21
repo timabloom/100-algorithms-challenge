4c371af96ab9e521d88185d6c55d7f883c1711cd
timabloom <timabloom@proton.me>
2024-02-21 13:00:10 +0100

    solved challenge 59

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,12 @@
 export function houseNumbersSum(inputArray: number[]): number {
+  let sum = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] !== 0) {
+      sum += inputArray[i];
+    } else break;
+  }
+  return sum;
 }
 
-// console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
