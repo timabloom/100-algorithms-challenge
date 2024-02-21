410bd4b1b984530fa497c4376f88c5866678ca04
timabloom <timabloom@proton.me>
2024-02-21 13:25:50 +0100

    solved challenge 60

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,15 @@
 export function houseOfCats(legs: number): number[] {
-
+  const humanLegsArray: number[] = [];
+  let count = 0;
+  for (let i = 0; i < legs; i += 2) {
+    if (i + 4 !== legs) {
+      humanLegsArray.push((count += 1));
+    } else {
+      count += 1;
+    }
+  }
+  return humanLegsArray;
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
