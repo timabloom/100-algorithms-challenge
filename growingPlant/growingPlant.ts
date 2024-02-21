c2930bce461f6779a1e247124b7596f6e69c7235
timabloom <timabloom@proton.me>
2024-02-21 12:51:50 +0100

    solved challenge 58

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,9 @@
-export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
-
+export function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  return Math.floor(desiredHeight / (upSpeed - downSpeed));
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
