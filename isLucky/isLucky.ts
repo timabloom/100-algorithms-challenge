24703361a850bd62f3d116ee76268c41aafb1408
timabloom <timabloom@proton.me>
2024-02-22 11:03:34 +0100

    solved challenge 65

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,21 @@
 export function isLucky(n: number): boolean {
+  const ticket = n.toString();
 
+  const startNumbers = ticket.slice(0, ticket.length / 2).split("");
+  const endNumbers = ticket.slice(ticket.length / 2).split("");
+
+  const sumStart: number = getSum(startNumbers);
+  const sumEnd: number = getSum(endNumbers);
+
+  return sumStart === sumEnd ? true : false;
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+function getSum(numbersArray: string[]) {
+  return numbersArray.reduce(
+    (acc: number, curr: string) => acc + parseInt(curr),
+    0
+  );
+}
+
+console.log(isLucky(1230));
+console.log(isLucky(239017));
