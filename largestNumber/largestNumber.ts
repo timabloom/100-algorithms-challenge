7e4823cf0fc6e0e15a90a35de69a7fda3d931418
timabloom <timabloom@proton.me>
2024-02-22 11:35:16 +0100

    solved challenge 67

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,9 @@
 export function largestNumber(n: number): number {
-
+  let numberString = "";
+  for (let i = 0; i < n; i++) {
+    numberString = numberString.concat("", "9");
+  }
+  return parseInt(numberString);
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));
