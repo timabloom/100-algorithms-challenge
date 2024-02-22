f5c3f21046d2d43718b6c48874880ca1e44c2403
timabloom <timabloom@proton.me>
2024-02-22 12:38:54 +0100

    solved challenge 69

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,23 @@
 export function lateRide(n: number): number {
+  const h = Math.floor(n / 60).toString();
+  const m = (n % 60).toString();
 
+  const hourFormated = addZeros(h);
+  const minutesFormated = addZeros(m);
+  const clockFormatTime = hourFormated.concat("", minutesFormated);
+
+  const clockFormatArray = clockFormatTime.split("");
+  const sumDigits = clockFormatArray.reduce(
+    (acc, curr) => acc + parseInt(curr),
+    0
+  );
+  return sumDigits;
 }
 
-// console.log(lateRide(240));
-// console.log(lateRide(808));
+function addZeros(time: string) {
+  if (time.length === 1) return (time = `0${time}`);
+  return time;
+}
+
+console.log(lateRide(240));
+console.log(lateRide(808));
