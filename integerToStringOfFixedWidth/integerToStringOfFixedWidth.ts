75b8ff00eadf11142129102f266ca4ea8998369a
timabloom <timabloom@proton.me>
2024-02-22 10:31:56 +0100

    solved challenge 64

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,22 @@
-export function integerToStringOfFixedWidth(number: number, width: number): string {
-
+export function integerToStringOfFixedWidth(
+  number: number,
+  width: number
+): string {
+  const digitsString = number.toString();
+  if (digitsString.length === width) {
+    return digitsString;
+  } else if (digitsString.length > width) {
+    return digitsString.substring(2);
+  } else {
+    const loopLength = width - digitsString.length;
+    let zeros = "";
+    for (let i = 0; i < loopLength; i++) {
+      zeros = zeros.concat("", "0");
+    }
+    return zeros.concat("", digitsString);
+  }
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+console.log(integerToStringOfFixedWidth(1234, 2));
+console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 5));
