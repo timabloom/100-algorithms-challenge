d9b04f741e9e9f961f04cd140d8c7d3326e942da
timabloom <timabloom@proton.me>
2024-02-22 09:59:51 +0100

    solved challenge 62

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,33 @@
-export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+export function incorrectPasscodeAttempts(
+  passcode: string,
+  attempts: string[]
+): boolean {
+  let incorrectAttemps = 0;
 
+  for (let i = 0; i < attempts.length; i++) {
+    if (attempts[i] === passcode) {
+      incorrectAttemps = 0;
+    } else {
+      incorrectAttemps += 1;
+    }
+    if (incorrectAttemps === 10) return true;
+  }
+  return false;
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(
+  incorrectPasscodeAttempts("1111", [
+    "1111",
+    "4444",
+    "9999",
+    "3333",
+    "8888",
+    "2222",
+    "7777",
+    "0000",
+    "6666",
+    "7285",
+    "5555",
+    "1111",
+  ])
+);
