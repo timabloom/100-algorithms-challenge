1f876a2dc4757ab41e520bdc6313a81626ff1f66
timabloom <timabloom@proton.me>
2024-02-22 11:28:10 +0100

    solved challenge 66

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,12 @@
 export function isTandemRepeat(inputString: string): boolean {
+if (inputString.length % 2) return false
 
+const startString = inputString.slice(inputString.length / 2)
+const endString = inputString.slice(0, inputString.length / 2)
+
+return startString === endString
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'))
+console.log(isTandemRepeat('qqq'))
+console.log(isTandemRepeat('2w2ww'))
