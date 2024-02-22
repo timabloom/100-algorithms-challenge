e3bd79550fb6fcdb9255697f96de491b6e4f4861
timabloom <timabloom@proton.me>
2024-02-22 12:41:13 +0100

    solved challenge 69 cleaner code

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -2,21 +2,11 @@ export function lateRide(n: number): number {
   const h = Math.floor(n / 60).toString();
   const m = (n % 60).toString();
 
-  const hourFormated = addZeros(h);
-  const minutesFormated = addZeros(m);
-  const clockFormatTime = hourFormated.concat("", minutesFormated);
+  const time = h.concat("", m);
 
-  const clockFormatArray = clockFormatTime.split("");
-  const sumDigits = clockFormatArray.reduce(
-    (acc, curr) => acc + parseInt(curr),
-    0
-  );
+  const timeArray = time.split("");
+  const sumDigits = timeArray.reduce((acc, curr) => acc + parseInt(curr), 0);
   return sumDigits;
-}
-
-function addZeros(time: string) {
-  if (time.length === 1) return (time = `0${time}`);
-  return time;
 }
 
 console.log(lateRide(240));
