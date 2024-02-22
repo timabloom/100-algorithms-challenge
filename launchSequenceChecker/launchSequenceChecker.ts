1ec8a6b3fa3984e2fc9b8ea58af9dfa4d61cd9ac
timabloom <timabloom@proton.me>
2024-02-22 13:26:07 +0100

    solved challenge 70

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,30 @@
-export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
-
+export function launchSequenceChecker(
+  systemNames: string[],
+  stepNumbers: number[]
+): boolean {
+  let sequence = {};
+  for (let i = 0; i < systemNames.length; i++) {
+    if (sequence.hasOwnProperty(systemNames[i])) {
+      if (sequence[systemNames[i]] >= stepNumbers[i]) {
+        return false;
+      }
+      sequence[systemNames[i]] = stepNumbers[i];
+    } else {
+      sequence[systemNames[i]] = stepNumbers[i];
+    }
+  }
+  return true;
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+console.log(
+  launchSequenceChecker(
+    ["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"],
+    [1, 10, 11, 2, 12, 111]
+  )
+);
+console.log(
+  launchSequenceChecker(
+    ["stage_1", "stage_1", "stage_2", "dragon"],
+    [2, 1, 12, 111]
+  )
+);
