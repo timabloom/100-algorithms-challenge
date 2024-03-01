8d5d1ade330f09b1920f502ad53b65c5b2d4205c
timabloom <timabloom@proton.me>
2024-03-01 13:35:22 +0100

    solved challenge 72

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,18 @@
 export function longestDigitsPrefix(inputString: string): string {
+  const digits = inputString.search(/[0-9]/);
+  if (digits === -1) return "";
 
+  const charactersArray = inputString.split("");
+  let lettersRemoved = "";
+  for (const character of charactersArray) {
+    const digit = character.search(/[0-9]/);
+    if (digit !== -1) {
+      lettersRemoved = `${lettersRemoved}${character}`;
+    } else {
+      lettersRemoved = `${lettersRemoved}_`;
+    }
+  }
+  return lettersRemoved.split("_").sort((a, b) => b.length - a.length)[0];
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix("123aa1"));
