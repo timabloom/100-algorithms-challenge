db2ccaf23768a7e0fa29a3eabb66602cb76998a6
timabloom <timabloom@proton.me>
2024-03-08 12:50:40 +0100

    solved challenge 75

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,6 @@
 export function maxMultiple(divisor: number, bound: number): number {
-
+  let remainder = bound % divisor;
+  return bound - remainder;
 }
 
-// console.log(maxMultiple(3,10));
+console.log(maxMultiple(3, 10));
