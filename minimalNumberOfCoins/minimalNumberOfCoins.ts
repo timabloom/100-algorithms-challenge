c938f1b77164b4cde9245f8503ac28e7c458e9c5
timabloom <timabloom@proton.me>
2024-03-08 15:06:40 +0100

    solved challenge 77

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,22 @@
 export function minimalNumberOfCoins(coins: number[], price: number): number {
+  const coinsSorted = coins.sort((a, b) => a - b);
+  let nextCoin = coinsSorted.length - 1;
+  let remaining = price;
+  let count = 0;
+  while (remaining > 0) {
+    const divideOf = remaining;
+    remaining = remaining % coinsSorted[nextCoin];
+    const divide = divideOf - remaining;
+    const amount = divide / coinsSorted[nextCoin];
+    count += amount;
+    nextCoin--;
+    if (nextCoin < 0) {
+      count++;
+      break;
+    }
+  }
 
+  return count;
 }
 
-// console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 2, 10], 28));
