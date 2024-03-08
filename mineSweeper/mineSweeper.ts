b1a9255890832bf54e3495f22c19b3683569918f
timabloom <timabloom@proton.me>
2024-03-08 14:09:02 +0100

    solved challenge 76

--- mineSweeper/mineSweeper.ts
+++ mineSweeper/mineSweeper.ts
@@ -1,7 +1,61 @@
 export function minesweeper(matrix: boolean[][]): number[][] {
-
+  const matrixNumbers: number[][] = [];
+  for (let x = 0; x < matrix[0].length; x++) {
+    for (let y = 0; y < matrix.length; y++) {
+      let count = 0;
+      if (x === 0) {
+        matrixNumbers.push([]);
+      }
+      if (y + 1 < matrix.length) {
+        if (matrix[y + 1][x]) {
+          count++;
+        }
+      }
+      if (y - 1 >= 0) {
+        if (matrix[y - 1][x]) {
+          count++;
+        }
+      }
+      if (x + 1 < matrix[0].length) {
+        if (matrix[y][x + 1]) {
+          count++;
+        }
+      }
+      if (x - 1 >= 0) {
+        if (matrix[y][x - 1]) {
+          count++;
+        }
+      }
+      if (y + 1 < matrix.length && x + 1 < matrix[0].length) {
+        if (matrix[y + 1][x + 1]) {
+          count++;
+        }
+      }
+      if (y - 1 >= 0 && x + 1 < matrix[0].length) {
+        if (matrix[y - 1][x + 1]) {
+          count++;
+        }
+      }
+      if (y + 1 < matrix.length && x - 1 >= 0) {
+        if (matrix[y + 1][x - 1]) {
+          count++;
+        }
+      }
+      if (y - 1 >= 0 && x - 1 >= 0) {
+        if (matrix[y - 1][x - 1]) {
+          count++;
+        }
+      }
+      matrixNumbers[y].push(count);
+    }
+  }
+  return matrixNumbers;
 }
 
-// console.log(minesweeper([[true, false, false],
-// [false, true, false],
-// [false, false, false]]));
+console.log(
+  minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, false],
+  ])
+);
