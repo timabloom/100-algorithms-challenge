a79339ce66544c1aa90b0da0ffa73cbb5c4ffe76
timabloom <timabloom@proton.me>
2024-03-13 15:02:42 +0100

    solved challenge 81

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,19 @@
-export function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
-
+export function pagesNumberingWithInk(
+  current: number,
+  numberOfDigits: number
+): number {
+  const startingAmount = current - 1;
+  let maxDigits = "";
+  const printedPages: number[] = [];
+  for (let i = 0; i < numberOfDigits; i++) {
+    maxDigits = maxDigits + current;
+    if (maxDigits.length > numberOfDigits) break;
+    printedPages.push(current);
+    current++;
+  }
+  return printedPages.length + startingAmount;
 }
 
-// console.log(pagesNumberingWithInk('G'));
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));
