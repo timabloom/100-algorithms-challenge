d1b4388e3171994fd73485b956ab1c726a73abb5
timabloom <timabloom@proton.me>
2024-03-13 16:09:23 +0100

    solved challenge 82

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,27 @@
 export function palindromeRearranging(inputString: string): boolean {
-
+  const inputArray = inputString.split("");
+  const sortedInput = {};
+  for (let i = 0; i < inputArray.length; i++) {
+    if (sortedInput.hasOwnProperty(inputArray[i])) {
+      sortedInput[inputArray[i]] = sortedInput[inputArray[i]] + 1;
+    } else {
+      sortedInput[inputArray[i]] = 1;
+    }
+  }
+  let oddNumbered = 0;
+  for (let letter in sortedInput) {
+    console.log(letter);
+    if (sortedInput[letter] % 2 !== 0) {
+      oddNumbered++;
+    }
+  }
+  if (
+    (inputArray.length % 2 === 0 && oddNumbered === 0) ||
+    (inputArray.length % 2 !== 0 && oddNumbered === 1)
+  ) {
+    return true;
+  }
+  return false;
 }
 
-// console.log(palindromeRearranging('aabb'));
+console.log(palindromeRearranging("aabb"));
