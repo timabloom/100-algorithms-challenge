4def12b09da35ed8447381997eb85cba2233f0ad
timabloom <timabloom@proton.me>
2024-03-13 11:31:50 +0100

    solved challenge 79

--- mostFrequentDigitSum/mostFrequentDigitSum.ts
+++ mostFrequentDigitSum/mostFrequentDigitSum.ts
@@ -1,6 +1,34 @@
 export function mostFrequentDigitSum(n: number): number {
+  const sequence = {};
+  let digits = n.toString().split("");
+  let x = n;
+  while (x > 0) {
+    const sum = digits.reduce((acc, curr) => acc + parseInt(curr), 0);
+    x = x - sum;
+    digits = x.toString().split("");
+    if (sequence.hasOwnProperty(sum)) {
+      sequence[sum] = sequence[sum] + 1;
+    } else {
+      sequence[sum] = 1;
+    }
+  }
 
+  let max = 0;
+  let maxValues: number[] = [];
+
+  for (let value in sequence) {
+    if (sequence[value] > max) {
+      max = sequence[value];
+      maxValues = [];
+      maxValues.push(parseInt(value));
+    } else if (sequence[value] === max) {
+      maxValues.push(parseInt(value));
+    }
+  }
+
+  maxValues.sort((a, b) => b - a);
+  return maxValues[0];
 }
 
-// console.log(mostFrequentDigitSum(88));
-// console.log(mostFrequentDigitSum(8));
+console.log(mostFrequentDigitSum(88));
+console.log(mostFrequentDigitSum(8));
