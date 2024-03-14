326a6cc866aa853c8460106a22f20dc3828884f4
timabloom <timabloom@proton.me>
2024-03-14 14:59:51 +0100

    solved challenge 83

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,30 @@
 export function pigLatin(str: string): string {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const strArray = str.split("");
+  const pigLatin = [...strArray];
+  let vowelFirst = false;
+  let consonantCluster = true;
 
+  for (let i = 0; i < str.length; i++) {
+    const vowel = vowels.find((consonant) => consonant === strArray[i]);
+    if (vowel && i === 0) {
+      vowelFirst = true;
+    } else if (vowel) {
+      consonantCluster = false;
+    } else if (consonantCluster) {
+      pigLatin.push(strArray[i]);
+      pigLatin.shift();
+    }
+  }
+
+  if (vowelFirst) {
+    pigLatin.push("way");
+  } else {
+    pigLatin.push("ay");
+  }
+
+  return pigLatin.join("");
 }
 
-// console.log(pigLatin("glove"));
-// console.log(pigLatin("eight"));
+console.log(pigLatin("glove"));
+console.log(pigLatin("eight"));
