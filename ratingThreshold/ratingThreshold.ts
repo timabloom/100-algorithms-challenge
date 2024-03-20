1a71d2328373b2aa43e7f2a770464252220b7b3b
timabloom <timabloom@proton.me>
2024-03-20 15:23:52 +0100

    solved challenge 86

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,16 @@
-export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
-
+export function ratingThreshold(
+  threshold: number,
+  ratings: number[][]
+): number[] {
+  const prosBelowThreshold: number[] = [];
+  for (let i = 0; i < ratings.length; i++) {
+    const rating =
+      ratings[i].reduce((acc, curr) => acc + curr, 0) / ratings[i].length;
+    if (rating < threshold) {
+      prosBelowThreshold.push(i);
+    }
+  }
+  return prosBelowThreshold;
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5, [[3, 4], [3, 3, 3, 4], [4]]));
