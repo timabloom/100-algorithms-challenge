f099fe3ec9b7a74bcdbe0e5540d16e11b6e40ef8
timabloom <timabloom@proton.me>
2024-03-20 15:09:18 +0100

    solved challenge 85

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,8 @@
 export function properNounCorrection(noun: string): string {
-
+  const nounUpperCase = noun.toLocaleUpperCase().slice(0, 1);
+  const nounLowerCase = noun.toLocaleLowerCase().slice(1);
+  return nounUpperCase.concat("", nounLowerCase);
 }
 
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection("pARiS"));
+console.log(properNounCorrection("John"));
