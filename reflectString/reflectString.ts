aa70def18ad834f249bef2d768e73a1496eea263
timabloom <timabloom@proton.me>
2024-03-20 15:47:26 +0100

    solved challenge 87

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,39 @@
 export function reflectString(inputString: string): string {
-
+  const alphabetReflection = {
+    a: "z",
+    b: "y",
+    c: "x",
+    d: "w",
+    e: "v",
+    f: "u",
+    g: "t",
+    h: "s",
+    i: "r",
+    j: "q",
+    k: "p",
+    l: "o",
+    m: "n",
+    n: "m",
+    o: "l",
+    p: "k",
+    q: "j",
+    r: "i",
+    s: "h",
+    t: "g",
+    u: "f",
+    v: "e",
+    w: "d",
+    x: "c",
+    y: "b",
+    z: "a",
+  };
+  const letters = inputString.split("");
+  let reflection = "";
+  for (let i = 0; i < letters.length; i++) {
+    const letterReflection: string = alphabetReflection[letters[i]];
+    reflection += letterReflection;
+  }
+  return reflection;
 }
 
-// console.log(reflectString("name"));
+console.log(reflectString("name"));
