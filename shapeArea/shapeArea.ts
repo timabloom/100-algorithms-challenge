7563acbb45abfb8c5fe98aa04176152f63eb0736
timabloom <timabloom@proton.me>
2024-03-26 14:34:01 +0100

    solved challenge 91

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,6 +1,14 @@
 export function shapeArea(n: number): number {
-
+  if (n === 1) return 1;
+  let area = 1;
+  let expansion = 4;
+  for (let i = 2; i <= n; i++) {
+    area += expansion;
+    expansion += 4;
+  }
+  return area;
 }
 
-// console.log(shapeArea(2));
-// console.log(shapeArea(3));
+console.log(shapeArea(2));
+console.log(shapeArea(3));
+console.log(shapeArea(4));
