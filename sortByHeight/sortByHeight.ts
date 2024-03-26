ccd924cf5ef1924d0ffecce9c013fd442699c250
timabloom <timabloom@proton.me>
2024-03-26 14:50:12 +0100

    solved challenge 92

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,15 @@
 export function sortByHeight(a: number[]): number[] {
-
+  const humans = a.filter((value) => value !== -1).sort((a, b) => a - b);
+  const sortedArray = a.map((element) => {
+    if (element !== -1) {
+      const human = humans[0];
+      humans.shift();
+      return (element = human);
+    } else {
+      return element;
+    }
+  });
+  return sortedArray;
 }
 
-// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
