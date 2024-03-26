2b4a27801b18d19ad4cb474f615650f275af21ce
timabloom <timabloom@proton.me>
2024-03-26 14:11:42 +0100

    solved challenge 90

--- seekAndDestroy/seekAndDestroy.ts
+++ seekAndDestroy/seekAndDestroy.ts
@@ -1,6 +1,10 @@
 export function seekAndDestroy(arr1: number[], arr2: number[]): number[] {
-
+  return arr1.filter((element) => {
+    if (!arr2.includes(element)) {
+      return element;
+    }
+  });
 }
 
-// console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
-// console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));
+console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
+console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));
