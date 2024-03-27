4f3c86e283570f35fbb71b861654b6a1f15c2132
timabloom <timabloom@proton.me>
2024-03-27 12:03:03 +0100

    solved challenge 94

--- squareDigitsSequence/squareDigitsSequence.ts
+++ squareDigitsSequence/squareDigitsSequence.ts
@@ -1,7 +1,18 @@
 export function squareDigitsSequence(a0: number): number {
-
+  const sums: number[] = [];
+  let sum = a0;
+  let iterations = 1;
+  while (!sums.includes(sum)) {
+    sums.push(sum);
+    const digits: string[] = sum.toString().split("");
+    sum = digits.reduce(
+      (acc, digit) => acc + parseInt(digit) * parseInt(digit),
+      0
+    );
+    iterations++;
+  }
+  return iterations;
 }
 
-
-// console.log(squareDigitsSequence(16));
-// console.log(squareDigitsSequence(103));
+console.log(squareDigitsSequence(16));
+console.log(squareDigitsSequence(103));
