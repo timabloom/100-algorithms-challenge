9396cdb061c45ac3af1e60014f7356930c01eead
timabloom <timabloom@proton.me>
2024-03-27 13:17:51 +0100

    solved challenge 96

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,27 @@
 export function stringsConstruction(a: string, b: string): number {
-
+  const letterDuplicates = addDuplicateChar(b);
+  const letterPatternDuplicates = addDuplicateChar(a);
+  const pattern = a.split("");
+  const copies: number[] = [];
+  for (let i = 0; i < pattern.length; i++) {
+    const amount = Math.floor(
+      letterDuplicates[pattern[i]] / letterPatternDuplicates[pattern[i]]
+    );
+    copies.push(amount);
+  }
+  return Math.min(...copies);
 }
 
-// console.log(stringsConstruction('abc', 'abccba'));
+function addDuplicateChar(letters: string) {
+  const duplicates = {};
+  for (let letter of letters.split("")) {
+    if (!duplicates.hasOwnProperty(letter)) {
+      duplicates[letter] = 1;
+    } else {
+      duplicates[letter] += 1;
+    }
+  }
+  return duplicates;
+}
+
+console.log(stringsConstruction("abc", "abccba"));
