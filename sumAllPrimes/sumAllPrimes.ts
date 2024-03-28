126a9979aec67f1b7f26a375740fe355349e958f
timabloom <timabloom@proton.me>
2024-03-28 13:45:16 +0100

    solved challenge 97 one less for loop

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,10 +1,8 @@
 export function sumAllPrimes(num: number): number {
   let sumAllNumbers = 0;
-  for (let i = 2; i <= num; i++) {
-    sumAllNumbers += i;
-  }
   let sumNotPrime = 0;
   for (let i = 2; i <= num; i++) {
+    sumAllNumbers += i;
     for (let j = 2; j < i; j++) {
       if (i % j === 0) {
         sumNotPrime += i;
