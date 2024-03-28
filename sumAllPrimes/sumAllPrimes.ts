36ae1dd7cd4611b857cafa6faf78d16fc8a8e0a1
timabloom <timabloom@proton.me>
2024-03-28 13:43:48 +0100

    solved challenge 97

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,6 +1,19 @@
 export function sumAllPrimes(num: number): number {
-
+  let sumAllNumbers = 0;
+  for (let i = 2; i <= num; i++) {
+    sumAllNumbers += i;
+  }
+  let sumNotPrime = 0;
+  for (let i = 2; i <= num; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        sumNotPrime += i;
+        break;
+      }
+    }
+  }
+  return sumAllNumbers - sumNotPrime;
 }
 
-// console.log(sumAllPrimes(10));
-// console.log(sumAllPrimes(977));
+console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(977));
