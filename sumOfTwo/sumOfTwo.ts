010a540f07899cd5053641eeeb077319b012bbd8
timabloom <timabloom@proton.me>
2024-03-29 15:48:36 +0100

    solved challenge 99

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,12 @@
 export function sumOfTwo(a: number[], b: number[], v: number): boolean {
-
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] + b[j] === v) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-// console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
