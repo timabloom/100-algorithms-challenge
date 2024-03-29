fe22666e93ad54d1e51c8b7c07462a615f439153
timabloom <timabloom@proton.me>
2024-03-29 16:58:46 +0100

    solved challenge 103

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,13 @@
 export function validTime(time: string): boolean {
-
+  const [h, m] = time.split(":");
+  if (parseInt(h) > 23 || parseInt(h) < 0) {
+    return false;
+  } else if (parseInt(m) > 59 || parseInt(m) < 0) {
+    return false;
+  }
+  return true;
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime("13:58"));
+console.log(validTime("25:51"));
+console.log(validTime("02:76"));
