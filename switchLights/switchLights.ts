443f66c9d29439d88394fa6a78a87b14e642991c
timabloom <timabloom@proton.me>
2024-03-29 16:09:53 +0100

    solved challenge 100

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,6 +1,18 @@
 export function switchLights(a: number[]): number[] {
-
+  const lights = [...a];
+  for (let i = 0; i < lights.length; i++) {
+    if (lights[i] === 1) {
+      for (let j = i; j >= 0; j--) {
+        if (lights[j] === 0) {
+          lights[j] = 1;
+        } else {
+          lights[j] = 0;
+        }
+      }
+    }
+  }
+  return lights;
 }
 
-// console.log(switchLights([1, 1, 1, 1, 1]));
-// console.log(switchLights([0, 0]));
+console.log(switchLights([1, 1, 1, 1, 1]));
+console.log(switchLights([0, 0]));
