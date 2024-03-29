590fcf9c760d4700c4872219a4a44af07e01f18f
timabloom <timabloom@proton.me>
2024-03-29 16:45:04 +0100

    solved challenge 102

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,13 @@
 export function uniqueDigitProducts(a: number[]): number {
+  const uniqueProducts: number[] = [];
+  for (let i = 0; i < a.length; i++) {
+    const digits = a[i].toString().split("");
+    const product = digits.reduce((acc, curr) => acc * parseInt(curr), 1);
+    if (!uniqueProducts.includes(product)) {
+      uniqueProducts.push(product);
+    }
+  }
+  return uniqueProducts.length;
 }
 
-// console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
